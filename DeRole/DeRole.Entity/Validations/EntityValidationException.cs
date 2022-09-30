e7dcffdfe91a684d6c4596a45aca3823e1e9890d
Felipe Poletti <felipepolettig@gmail.com>
2022-09-30 16:45:45 -0300

    create user entity and validation

---
+++ DeRole/DeRole.Entity/Validations/EntityValidationException.cs
@@ -0,0 +1,14 @@
+﻿namespace DeRole.Entity.Validations
+{
+    public class EntityValidationException : Exception
+    {
+        public EntityValidationException(string error) : base(error)
+        { }
+
+        public static void When(bool hasError, string errorMessage)
+        {
+            if (hasError)
+                throw new EntityValidationException(errorMessage);
+        }
+    }
+}
