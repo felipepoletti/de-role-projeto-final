36b70c94db7fe209d75f6e37029a0ab6e11931c9
Felipe Poletti <felipepolettig@gmail.com>
2022-11-07 20:55:10 -0300

    create dbcontext and mapping person

---
+++ DeRole/DeRole.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using DeRole.Entity.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeRole.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { }
+
+        public DbSet<User>  Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
