2fa9b51fc2d833627b1da27b06a5b42322ffef52
Felipe Poletti <felipepolettig@gmail.com>
2022-11-07 22:38:48 -0300

    create map for like, event and eventType

---
+++ DeRole/DeRole.Data/Context/Mapping/EventMap.cs
@@ -0,0 +1,52 @@
+﻿using DeRole.Entity.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DeRole.Data.Context.Mapping
+{
+    public class EventMap : IEntityTypeConfiguration<Event>
+    {
+        public void Configure(EntityTypeBuilder<Event> builder) 
+        {
+            builder.ToTable("EVENTO");
+
+            builder.HasKey(e => e.Id);
+
+            builder.Property(e => e.Id)
+                .HasColumnName("EVENTO_ID");
+
+            builder.Property(e => e.EventName)
+                .HasColumnName("EVENTO_NAME");
+
+            builder.Property(e => e.Price)
+                .HasColumnName("EVENTO_PRECO");
+
+            builder.Property(e => e.EventDescription)
+                .HasColumnName("EVENTO_DESCRICAO");
+
+            builder.Property(e => e.Date)
+                .HasColumnName("EVENTO_DATA");
+
+            builder.Property(e => e.Address)
+                .HasColumnName("EVENTO_ENDERECO");
+
+            builder.Property(e => e.AddressNumber)
+                .HasColumnName("EVENTO_ENDERECO_NUMERO");
+
+            builder.Property(e => e.AddressComplement)
+                .HasColumnName("EVENTO_ENDERECO_COMPLEMENTO");
+
+            builder.Property(e => e.EventTypeId)
+                .HasColumnName("TIPO_EVENTO_ID");
+
+            builder.Property(e => e.UserId)
+                .HasColumnName("PESSOA_ID");
+
+            builder.HasOne(e => e.User)
+                .WithMany(t => t.Events);
+
+            builder.HasOne(e => e.EventType)
+                .WithOne(t => t.Event);
+        }
+    }
+}
---
+++ DeRole/DeRole.Data/Context/Mapping/LikeMap.cs
@@ -0,0 +1,26 @@
+﻿using DeRole.Entity.Domain;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeRole.Data.Context.Mapping
+{
+    public class LikeMap : IEntityTypeConfiguration<Like>
+    {
+        public void Configure(EntityTypeBuilder<Like> builder)
+        {
+            builder.ToTable("EVENTO_USUARIO_LIKE");
+
+            builder.HasKey(u => u.Id);
+
+            builder.Property(u => u.UserId)
+                .HasColumnName("USUARIO_ID");
+
+            builder.Property(u => u.EventId)
+                .HasColumnName("EVENTO_ID");
+
+            builder.HasOne(e => e.User)
+                .WithMany(u => u.Likes)
+                .HasForeignKey(e => e.UserId);
+        }
+    }
+}
